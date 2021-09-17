7161c5755733124bc6a909f7e1e9772b9f41bb6d
cztan <cyztan@gmail.com>
2021-09-17 16:47:26 -0700

    Change to MRSA CMTF

--- tensorpac/cmtf.py
+++ tensorpac/cmtf.py
@@ -1,17 +1,19 @@
 """
 Coupled Matrix Tensor Factorization
 """
+
 import numpy as np
 import tensorly as tl
 from tensorly.tenalg import khatri_rao
-from statsmodels.multivariate.pca import PCA
 from copy import deepcopy
+from tensorly.decomposition._cp import initialize_cp, parafac
+from .soft_impute import SoftImpute
 
 
 tl.set_backend('numpy')
 
 
-def buildMatrix(tFac):
+def buildMat(tFac):
     """ Build the matrix in CMTF from the factors. """
     return tFac.factors[0] @ tFac.mFactor.T
 
@@ -24,13 +26,15 @@ def calcR2X(tFac, tIn=None, mIn=None):
 
     if tIn is not None:
         tMask = np.isfinite(tIn)
-        vTop += np.sum(np.square(tl.cp_to_tensor(tFac) * tMask - np.nan_to_num(tIn)))
-        vBottom += np.sum(np.square(np.nan_to_num(tIn)))
+        tIn = np.nan_to_num(tIn)
+        vTop += np.linalg.norm(tl.cp_to_tensor(tFac) * tMask - tIn)**2.0
+        vBottom += np.linalg.norm(tIn)**2.0
     if mIn is not None:
         mMask = np.isfinite(mIn)
-        recon = tFac if isinstance(tFac, np.ndarray) else buildMatrix(tFac)
-        vTop += np.sum(np.square(recon * mMask - np.nan_to_num(mIn)))
-        vBottom += np.sum(np.square(np.nan_to_num(mIn)))
+        recon = tFac if isinstance(tFac, np.ndarray) else buildMat(tFac)
+        mIn = np.nan_to_num(mIn)
+        vTop += np.linalg.norm(recon * mMask - mIn)**2.0
+        vBottom += np.linalg.norm(mIn)**2.0
 
     return 1.0 - vTop / vBottom
 
@@ -47,41 +51,42 @@ def tensor_degFreedom(tFac) -> int:
 
 def reorient_factors(tFac):
     """ This function ensures that factors are negative on at most one direction. """
-    # Flip the subjects to be positive
+    # Flip the types to be positive
+    tMeans = np.sign(np.mean(tFac.factors[2], axis=0))
+    tFac.factors[1] *= tMeans[np.newaxis, :]
+    tFac.factors[2] *= tMeans[np.newaxis, :]
+
+    # Flip the cytokines to be positive
     rMeans = np.sign(np.mean(tFac.factors[1], axis=0))
-    agMeans = np.sign(np.mean(tFac.factors[2], axis=0))
-    tFac.factors[0] *= rMeans[np.newaxis, :] * agMeans[np.newaxis, :]
+    tFac.factors[0] *= rMeans[np.newaxis, :]
     tFac.factors[1] *= rMeans[np.newaxis, :]
-    tFac.factors[2] *= agMeans[np.newaxis, :]
 
     if hasattr(tFac, 'mFactor'):
-        tFac.mFactor *= rMeans[np.newaxis, :] * agMeans[np.newaxis, :]
-
+        tFac.mFactor *= rMeans[np.newaxis, :]
     return tFac
-
-
-def totalVar(tFac):
-    """ Total variance of a factorization on reconstruction. """
-    varr = tl.cp_norm(tFac)
-    if hasattr(tFac, 'mFactor'):
-        varr += tl.cp_norm((None, [tFac.factors[0], tFac.mFactor]))
-    return varr
 
 
 def sort_factors(tFac):
     """ Sort the components from the largest variance to the smallest. """
-    rr = tFac.rank
     tensor = deepcopy(tFac)
-    vars = np.array([totalVar(delete_component(tFac, np.delete(np.arange(rr), i))) for i in np.arange(rr)])
-    order = np.flip(np.argsort(vars))
 
+    # Variance separated by component
+    norm = np.copy(tFac.weights)
+    for factor in tFac.factors:
+        norm *= np.sum(np.square(factor), axis=0)
+
+    # Add the variance of the matrix
+    if hasattr(tFac, 'mFactor'):
+        norm += np.sum(np.square(tFac.factors[0]), axis=0) * np.sum(np.square(tFac.mFactor), axis=0)
+
+    order = np.flip(np.argsort(norm))
     tensor.weights = tensor.weights[order]
     tensor.factors = [fac[:, order] for fac in tensor.factors]
-    np.testing.assert_allclose(tl.cp_to_tensor(tFac), tl.cp_to_tensor(tensor))
+    np.testing.assert_allclose(tl.cp_to_tensor(tFac), tl.cp_to_tensor(tensor), atol=1e-9)
 
     if hasattr(tFac, 'mFactor'):
         tensor.mFactor = tensor.mFactor[:, order]
-        np.testing.assert_allclose(buildMatrix(tFac), buildMatrix(tensor))
+        np.testing.assert_allclose(buildMat(tFac), buildMat(tensor), atol=1e-9)
 
     return tensor
 
@@ -126,7 +131,7 @@ def censored_lstsq(A: np.ndarray, B: np.ndarray, uniqueInfo) -> np.ndarray:
         uu = np.squeeze(unique[:, i])
 
         Bx = B[uu, :]
-        X[:, uI] = np.linalg.lstsq(A[uu, :], Bx[:, uI], rcond=None)[0]
+        X[:, uI] = np.linalg.lstsq(A[uu, :], Bx[:, uI], rcond=-1)[0]
     return X.T
 
 
@@ -154,83 +159,62 @@ def initialize_cp(tensor: np.ndarray, matrix: np.ndarray, rank: int):
     factors : CPTensor
         An initial cp tensor.
     """
-    factors = []
-    for mode in range(tl.ndim(tensor)):
-        unfold = tl.unfold(tensor, mode)
+    factors = [np.ones((tensor.shape[i], rank)) for i in range(tensor.ndim)]
 
-        if mode == 0 and (matrix is not None):
-            unfold = np.hstack((unfold, matrix))
+    # SVD init mode 0
+    unfold = tl.unfold(tensor, 0)
+    unfold = np.hstack((unfold, matrix))
 
-        # Remove completely missing columns
-        unfold = unfold[:, np.sum(np.isfinite(unfold), axis=0) > 2]
+    si = SoftImpute(J=rank)
+    si.fit(unfold)
 
-        # Impute by PCA
-        outt = PCA(unfold, ncomp=1, method="nipals", missing="fill-em", standardize=False, demean=False, normalize=False, max_em_iter=1000)
-        recon_pca = outt.scores @ outt.loadings.T
-        unfold[np.isnan(unfold)] = recon_pca[np.isnan(unfold)]
+    factors[0] = si.u
 
-        U = np.linalg.svd(unfold)[0]
-
-        if U.shape[1] < rank:
-            # This is a hack but it seems to do the job for now
-            pad_part = np.random.rand(U.shape[0], rank - U.shape[1])
-            U = tl.concatenate([U, pad_part], axis=1)
-
-        factors.append(U[:, :rank])
-
+    unfold = tl.unfold(tensor, 1)
+    unfold = unfold[:, np.all(np.isfinite(unfold), axis=0)]
+    factors[1] = np.linalg.svd(unfold)[0]
+    factors[1] = factors[1][:, :rank]
     return tl.cp_tensor.CPTensor((None, factors))
 
 
-def perform_CMTF(tOrig, mOrig=None, r=3):
+def perform_CMTF(tOrig, mOrig=None, r=9):
     """ Perform CMTF decomposition. """
-
+    assert tOrig.dtype == float
+    if mOrig is not None:
+        assert mOrig.dtype == float
     tFac = initialize_cp(tOrig, mOrig, r)
 
     # Pre-unfold
-    unfolded = [tl.unfold(tOrig, i) for i in range(tOrig.ndim)]
-
-    if mOrig is not None:
-        uniqueInfoM = np.unique(np.isfinite(mOrig), axis=1, return_inverse=True)
-        tFac.mFactor = censored_lstsq(tFac.factors[0], mOrig, uniqueInfoM)
-        unfolded[0] = np.hstack((unfolded[0], mOrig))
-
-    R2X_last = -np.inf
-    tFac.R2X = calcR2X(tFac, tOrig, mOrig)
+    unfolded = np.hstack((tl.unfold(tOrig, 0), mOrig))
+    missingM = np.all(np.isfinite(mOrig), axis=1)
+    R2X = -np.inf
 
     # Precalculate the missingness patterns
-    uniqueInfo = [np.unique(np.isfinite(B.T), axis=1, return_inverse=True) for B in unfolded]
+    uniqueInfo = np.unique(np.isfinite(unfolded.T), axis=1, return_inverse=True)
 
-    for ii in range(2000):
-        # Solve for the shared mode matrix
+    for _ in range(40):
+        tensor = np.nan_to_num(tOrig) + tl.cp_to_tensor(tFac) * np.isnan(tOrig)
+        tFac = parafac(tensor, r, 200, init=tFac, verbose=False, fixed_modes=[0], mask=np.isfinite(tOrig))
+
+        # Solve for the glycan matrix fit
+        tFac.mFactor = np.linalg.lstsq(tFac.factors[0][missingM, :], mOrig[missingM, :], rcond=-1)[0].T
+
+        # Solve for subjects factors
         kr = khatri_rao(tFac.factors, skip_matrix=0)
+        kr = np.vstack((kr, tFac.mFactor))
+        tFac.factors[0] = censored_lstsq(kr, unfolded.T, uniqueInfo)
 
-        if mOrig is not None:
-            kr = np.vstack((kr, tFac.mFactor))
+        R2X_last = R2X
+        R2X = calcR2X(tFac, tOrig, mOrig)
+        assert R2X > 0.0
 
-        tFac.factors[0] = censored_lstsq(kr, unfolded[0].T, uniqueInfo[0])
-
-        # PARAFAC on other modes
-        for m in range(1, len(tFac.factors)):
-            kr = khatri_rao(tFac.factors, skip_matrix=m)
-            tFac.factors[m] = censored_lstsq(kr, unfolded[m].T, uniqueInfo[m])
-
-        # Solve for the matrix fit
-        if mOrig is not None:
-            tFac.mFactor = censored_lstsq(tFac.factors[0], mOrig, uniqueInfoM)
-
-        if ii % 2 == 0:
-            R2X_last = tFac.R2X
-            tFac.R2X = calcR2X(tFac, tOrig, mOrig)
-            assert tFac.R2X > 0.0
-
-        if tFac.R2X - R2X_last < 1e-6:
+        if R2X - R2X_last < 1e-6:
             break
 
     tFac = cp_normalize(tFac)
     tFac = reorient_factors(tFac)
-
-    if r > 1:
-        tFac = sort_factors(tFac)
+    tFac = sort_factors(tFac)
+    tFac.R2X = R2X
 
     print("R2X: " + str(tFac.R2X))
 
