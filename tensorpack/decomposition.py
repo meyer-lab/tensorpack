db7dad67b0d5c1631c33cd26a57993dddae3ab98
Cyrillus <cyztan@gmail.com>
2021-11-19 16:39:29 -0800

    Fix PCA to only one run

--- tensorpack/decomposition.py
+++ tensorpack/decomposition.py
@@ -15,17 +15,14 @@ class Decomposition():
         self.TR2X = [c.R2X for c in self.tfac]
         self.sizeT = [rr * sum(self.tfac[0].shape) for rr in self.rrs]
 
-    def perform_PCA(self):
-        ## insert PCA here
-        ## Assumes to keep dim=1 same
+    def perform_PCA(self, flattenon=0):
         dataShape = self.data.shape
-        flatData = np.reshape(self.data, (dataShape[0]*dataShape[2] , dataShape[1]))
-
-        self.PCA = [PCA(flatData, ncomp=rr, missing='fill-em', standardize=False, demean=False, normalize=False) for rr in self.rrs]
-        recon = [c.scores @ c.loadings.T for c in self.PCA]
+        flatData = np.reshape(np.moveaxis(self.data, flattenon, 0), (dataShape[flattenon], -1))
+        self.PCA = PCA(flatData, ncomp=max(self.rrs), missing='fill-em', standardize=False, demean=False, normalize=False)
+        recon = [self.PCA.scores[:, :rr] @ self.PCA.loadings.T[:rr, :] for rr in self.rrs]
         self.PCAR2X = [calcR2X(c, mIn = flatData) for c in recon]
         self.sizePCA = [sum(flatData.shape) * rr for rr in self.rrs]
-        pass
+
 
     def Q2X_chord(self, drop=10, repeat=10):
         self.chordQ2X = None # df
