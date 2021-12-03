14da78e99ae82ac1f4f09baaed847f2dd9228e5e
eniohodzic <ehodzic@g.ucla.edu>
2021-12-02 22:51:58 -0800

    Initial Q2X_chord method

--- tensorpack/decomposition.py
+++ tensorpack/decomposition.py
@@ -1,9 +1,11 @@
 import pickle
 import numpy as np
 import pandas as pd
+from pandas._libs import missing
+from pandas.core.frame import DataFrame
 from statsmodels.multivariate.pca import PCA
 from sklearn.decomposition import TruncatedSVD
-from .cmtf import perform_CP, calcR2X
+from tensorpack.cmtf import perform_CP, calcR2X
 
 
 class Decomposition():
@@ -30,9 +32,25 @@ class Decomposition():
         self.sizePCA = [sum(flatData.shape) * rr for rr in self.rrs]
 
 
-    def Q2X_chord(self, drop=10, repeat=10):
-        self.chordQ2X = None # df
-        pass
+    def Q2X_chord(self, drop=2, repeat=2):
+        Q2X = np.zeros((repeat,self.rrs[-1]))
+        for x in range(repeat):
+            missingCube = np.copy(self.data)
+            for _ in range(drop):
+                idxs = np.argwhere(np.isfinite(missingCube))
+                i, j, k = idxs[np.random.choice(idxs.shape[0], 1)][0]
+                missingCube[:, j, k] = np.nan
+            tenFacs = [self.method(missingCube, r=rr) for rr in self.rrs]
+            tImps = [c.to_tensor() for c in tenFacs]
+            tIn = np.copy(self.data)
+            tIn[np.isfinite(missingCube)] = np.nan
+            tMask = np.isfinite(tIn)
+            for c,tImp in enumerate(tImps):
+                Top = np.sum(np.square(tImp * tMask - np.nan_to_num(tIn)))
+                Bottom = np.sum(np.square(np.nan_to_num(tIn)))
+                Q2X[x,c] = 1 - Top/Bottom
+                
+        self.chordQ2X = Q2X # df
 
     def Q2X_entry(self, drop=10, repeat=10):
         self.entryQ2X = None  # df
