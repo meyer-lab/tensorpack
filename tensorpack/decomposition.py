6dd576428f5b516e36d22a61811837e94315e2d6
ehung <ethanhung11@example.com>
2022-01-28 15:53:16 -0800

    fixes to Q2X_entry

--- tensorpack/decomposition.py
+++ tensorpack/decomposition.py
@@ -1,18 +1,9 @@
-from os import remove
 import pickle
-from xml.etree.ElementTree import tostring
 import numpy as np
-import pandas as pd
 from numpy.linalg import norm
-from sklearn.decomposition import TruncatedSVD
+from tensorly import partial_svd
 from .cmtf import perform_CP, calcR2X
-
-def flatten_to_mat(tensor):
-    n = tensor.shape[0]
-    tflat = np.reshape(tensor, (n, -1))
-    if not np.all(np.isfinite(tflat)):
-        tflat = impute_missing_mat(tflat)
-    return tflat
+from .SVD_impute import IterativeSVD
 
 def impute_missing_mat(dat):
     miss_idx = np.where(~np.isfinite(dat))
@@ -27,22 +18,21 @@ def impute_missing_mat(dat):
 
     diff = 1.0
     while diff > 1e-3:
-        tsvd = TruncatedSVD(n_components=min(dat.shape)-1)
-        scores = tsvd.fit_transform(imp)
-        loadings = tsvd.components_
+        U, S, V = partial_svd(imp, min(dat.shape) - 1)
+        scores = U @ np.diag(S)
+        loadings = V
         recon = scores @ loadings
         new_diff = norm(imp[miss_idx] - recon[miss_idx]) / norm(recon[miss_idx])
-        assert new_diff < diff, "Matrix imputation difference is not decreasing"
         diff = new_diff
         imp[miss_idx] = recon[miss_idx]
     return imp
 
 
 class Decomposition():
-    def __init__(self, data, max_rr=6):
+    def __init__(self, data, max_rr=5):
         self.data = data
         self.method = perform_CP
-        self.rrs = np.arange(max_rr)
+        self.rrs = np.arange(1,max_rr+1)
         pass
 
     def perform_tfac(self):
@@ -50,15 +40,18 @@ class Decomposition():
         self.tR2X = [c.R2X for c in self.tfac]
         self.tsize = [rr * sum(self.tfac[0].shape) for rr in self.rrs]
 
-    def perform_PCA(self):
-        flatData = flatten_to_mat(self.data)
-        tsvd = TruncatedSVD(n_components=max(self.rrs))
-        scores = tsvd.fit_transform(flatData)
-        loadings = tsvd.components_
-        recon = [scores[:, :rr] @ loadings[:rr, :] for rr in self.rrs]
-        self.PCAR2X = [calcR2X(c, mIn = flatData) for c in recon]
-        self.sizePCA = [sum(flatData.shape) * rr for rr in self.rrs]
+    def perform_PCA(self, flattenon=0):
+        dataShape = self.data.shape
+        flatData = np.reshape(np.moveaxis(self.data, flattenon, 0), (dataShape[flattenon], -1))
+        if not np.all(np.isfinite(flatData)):
+            flatData = impute_missing_mat(flatData)
 
+        U, S, V = partial_svd(flatData, max(self.rrs))
+        scores = U @ np.diag(S)
+        loadings = V
+        recon = [scores[:, :rr] @ loadings[:rr, :] for rr in self.rrs]
+        self.PCAR2X = [calcR2X(c, mIn=flatData) for c in recon]
+        self.sizePCA = [sum(flatData.shape) * rr for rr in self.rrs]
 
     def Q2X_chord(self, drop=10, repeat=10):
         Q2X = np.zeros(repeat,self.rrs[-1])
@@ -73,15 +66,16 @@ class Decomposition():
             tImp = np.copy(self.data)
             tImp[np.isfinite(missingCube)] = np.nan
 
-            for rr in enumerate(self.rrs):
-                tFac = self.method(missingCube, rr)
+            for rr in self.rrs:
+                tFac = self.method(missingCube, r=rr)
                 Q2X[x,rr-1] = calcR2X(tFac, tIn=tImp)
                 
         self.chordQ2X = Q2X
 
-    def Q2X_entry(self, drop=10, repeat=10, comparePCA=True, flattenon=0):
-        Q2X = np.zeros((repeat,self.rrs[-1]+1))
-        Q2XPCA = np.zeros((repeat,self.rrs[-1]+1))
+    def Q2X_entry(self, drop=10, repeat=5, comparePCA=True, flattenon=0):
+        Q2X = np.zeros((repeat,self.rrs[-1]))
+        # Q2X = np.zeros((np.sum(np.isfinite(self.data)), self.rrs[-1]))
+        Q2XPCA = np.zeros((repeat,self.rrs[-1]))
         for x in range(repeat):
             missingCube = np.copy(self.data)
             tImp = np.copy(self.data)
@@ -109,56 +103,70 @@ class Decomposition():
             4)  add emin values back in missingCube
             """
 
-            chooseCube = np.isfinite(tImp)                                  # tensor of 0/1 showing present or missing values
-            idxs = np.argwhere(chooseCube)                                  # positions of finite values in original data
-            selectidxs = np.copy(idxs)
+            chooseCube = np.isfinite(tImp)
+            idxs = np.argwhere(chooseCube)
 
-            midxs = np.zeros((tImp.ndim,max(tImp.shape)))                   # array representing every chord
+            modeidxs = np.zeros((tImp.ndim,max(tImp.shape)))
+            # array representing the chords in every mode (row)
             for i in range(tImp.ndim):
-                midxs[i] = [1 for n in range(tImp.shape[i])] + [0 for m in range(len(midxs[i])-tImp.shape[i])]
+                modeidxs[i] = [1 for n in range(tImp.shape[i])] + [0 for m in range(len(modeidxs[i])-tImp.shape[i])]
+            # self.modeidxs = modeidxs
             
             counter = 0
-            while np.sum(midxs) > 0:                                        # number of columns with no values inside
+            while np.sum(modeidxs) > 0:
+                # number of columns with no values inside
                 ranidx = np.random.choice(idxs.shape[0], 1) 
                 i,j,k = idxs[ranidx][0]
-                if midxs[0,i] > 0 or midxs[1,j] > 0 or midxs[2,k] > 0:
-                    midxs[0,i] = 0
-                    midxs[1,j] = 0
-                    midxs[2,k] = 0
-                    selectidxs = np.delete(selectidxs, ranidx, 0)           # holds positions in idxs excluding emin
+                if modeidxs[0,i] > 0 or modeidxs[1,j] > 0 or modeidxs[2,k] > 0:
+                    if modeidxs[0,i] > 0:
+                        modeidxs[0,i] -= 1
+                    if modeidxs[1,j] > 0:
+                        modeidxs[1,j] -= 1
+                    if modeidxs[2,k] > 0:
+                        modeidxs[2,k] -= 1
+                    np.delete(idxs, ranidx, axis=0)
                     counter += 1
-                
-            print(str(counter) + " values removed")
-            print(str(selectidxs.shape[0]/idxs.shape[0]) + "percent of data remainins")
-            assert selectidxs.shape[0] >= drop                              # possible to remove drop request values?
+            
+            assert idxs.shape[0] >= drop
+            # testing       print(str(counter) + " values withheld from drop")
             
             for _ in range(drop):
-                i, j, k = selectidxs[np.random.choice(selectidxs.shape[0], 1)][0]
+                i, j, k = idxs[np.random.choice(idxs.shape[0], 1)][0]
                 missingCube[i,j,k] = np.nan
 
-
-
-
-            tImp[np.isfinite(missingCube)] = np.nan                         # run Q2X for tensor method
-            for rr in self.rrs:
-                tFac = self.method(missingCube, r=rr+1)
-                Q2X[x,rr] = calcR2X(tFac, tIn=tImp)
-
-            if comparePCA:                                                  # run Q2X for PCA
-                missingMat = flatten_to_mat(missingCube)
-                mImp = flatten_to_mat(self.data)
-                mImp[np.isfinite(missingCube)] = np.nan
-
-                tsvd = TruncatedSVD(n_components=max(self.rrs))
-                scores = tsvd.fit_transform(missingMat)
-                loadings = tsvd.components_
-                recon = [scores[:, :rr] @ loadings[:rr, :] for rr in self.rrs]
-                Q2XPCA[x,rr] = [calcR2X(c, mIn = mImp) for c in recon]
+            # print(np.sum(np.isnan(missingCube)) + " values dropped") # testing
             
-            print("Finished repetition: " + str(x))
+            for rr in self.rrs:
+                tFac = self.method(missingCube, r=rr)
+                Q2X[x,rr-1] = calcR2X(tFac, tIn=tImp)
+
+            """
+            # modified Q2X for testing single drops
+            count = 0
+            for id in idxs:
+                missingCube[id[0],id[1],id[2]] = np.nan
+                tImp[np.isfinite(missingCube)] = np.nan
+                for rr in self.rrs:
+                    tFac = self.method(missingCube, r=rr)
+                    Q2X[count,rr-1] = calcR2X(tFac, tIn=tImp)
+                count += 1
+            """
+            
+            if comparePCA:
+                si = IterativeSVD(rank=rank, random_state=1)
+                missingMat = np.reshape(np.moveaxis(missingCube, flattenon, 0), (missingCube.shape[flattenon], -1))
+                missingMat = impute_missing_mat(missingMat)
+                mImp = np.reshape(np.moveaxis(tImp, flattenon, 0), (tImp.shape[flattenon], -1))
+
+                U, S, V = partial_svd(missingMat, max(self.rrs))
+                scores = U @ np.diag(S)
+                loadings = V
+                recon = [scores[:, :rr] @ loadings[:rr, :] for rr in self.rrs]
+                Q2XPCA[x,:] = [calcR2X(c, mIn = mImp) for c in recon]
+                self.entryQ2XPCA = Q2XPCA
     
         self.entryQ2X = Q2X
-        self.entryQ2XPCA = Q2XPCA      
+        
 
     def save(self, pfile):
         with open(pfile, "wb") as output_file:
