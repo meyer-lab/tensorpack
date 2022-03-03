185d2d050cc901ae02bd01ece5a74bc8a5f41ae5
ehung <ethanhung11@example.com>
2022-03-03 15:09:50 -0800

    prelim plot functions

--- tensorpack/plot.py
+++ tensorpack/plot.py
@@ -3,83 +3,87 @@ This file makes all standard plots for tensor analysis
 """
 
 import numpy as np
+import pandas as pd
 from matplotlib.ticker import ScalarFormatter
 from .decomposition import Decomposition
 
 
-def tfacr2x(ax, decomp: Decomposition):
+def tfacr2x(ax, pos, decomp: Decomposition):
     """
     Plot R2X of tensor decomp with more components
     """
     comps = decomp.rrs
-    ax.scatter(comps, decomp.TR2X, s=10)
-    ax.set_ylabel("Tensor Fac R2X")
-    ax.set_xlabel("Number of Components")
-    ax.set_title("Variance explained by tensor decomposition")
-    ax.set_xticks([x for x in comps])
-    ax.set_xticklabels([x for x in comps])
-    ax.set_ylim(0, 1)
-    ax.set_xlim(0.5, np.amax(comps) + 0.5)
+    ax[pos].scatter(comps, decomp.TR2X, s=10)
+    ax[pos].set_ylabel("Tensor Fac R2X")
+    ax[pos].set_xlabel("Number of Components")
+    ax[pos].set_title("Variance explained by tensor decomposition")
+    ax[pos].set_xticks([x for x in comps])
+    ax[pos].set_xticklabels([x for x in comps])
+    ax[pos].set_ylim(0, 1)
+    ax[pos].set_xlim(0.5, np.amax(comps) + 0.5)
 
 
-def reduction(ax, decomp):
+def reduction(ax, pos, decomp):
     """
     Plot the reduced dataset size vs. (1-R2X), TFac vs. PCA
     """
-    # find attributes
     CPR2X, PCAR2X, sizeTfac, sizePCA = np.asarray(decomp.TR2X), np.asarray(decomp.PCAR2X), decomp.sizeT, decomp.sizePCA
-    ax.set_xscale("log", base=2)
-    ax.plot(sizeTfac, 1.0 - CPR2X, ".", label="TFac")
-    ax.plot(sizePCA, 1.0 - PCAR2X, ".", label="PCA")
-    ax.set_ylabel("Normalized Unexplained Variance")
-    ax.set_xlabel("Size of Reduced Data")
-    ax.set_title("Data reduction, TFac vs. PCA")
-    ax.set_ylim(bottom=0.0)
-    ax.xaxis.set_major_formatter(ScalarFormatter())
-    ax.legend()
+    ax[pos].set_xscale("log", base=2)
+    ax[pos].plot(sizeTfac, 1.0 - CPR2X, ".", label="TFac")
+    ax[pos].plot(sizePCA, 1.0 - PCAR2X, ".", label="PCA")
+    ax[pos].set_ylabel("Normalized Unexplained Variance")
+    ax[pos].set_xlabel("Size of Reduced Data")
+    ax[pos].set_title("Data reduction, TFac vs. PCA")
+    ax[pos].set_ylim(bottom=0.0)
+    ax[pos].xaxis.set_major_formatter(ScalarFormatter())
+    ax[pos].legend()
 
 
-def q2xchord(ax, decomp):
-    # figure 3a in MSB
+def q2xchord(ax, pos, decomp):
     chords_df = decomp.chordQ2X
     comps = decomp.rrs
-    chords_df = chords_df.groupby('Components').agg({'R2X': ['mean', 'sem']})
+    chords_df = pd.DataFrame(decomp.chordQ2X).T
+    chords_df.index = comps
+    chords_df['mean'] = chords_df.mean(axis=1)
+    chords_df['sem'] = chords_df.sem(axis=1)
 
-    Q2Xchord = chords_df['R2X']['mean']
-    Q2Xerrors = chords_df['R2X']['sem']
-    ax.scatter(comps, Q2Xchord, s=10)
-    ax.errorbar(comps, Q2Xchord, yerr=Q2Xerrors, fmt='none')
-    ax.set_ylabel("Q2X of Imputation")
-    ax.set_xlabel("Number of Components")
-    ax.set_xticks([x for x in comps])
-    ax.set_xticklabels([x for x in comps])
-    ax.set_ylim(0, 1)
-
-    pass
+    Q2Xchord = chords_df['mean']
+    Q2Xerrors = chords_df['sem']
+    ax[pos].scatter(comps, Q2Xchord, s=10)
+    ax[pos].errorbar(comps, Q2Xchord, yerr=Q2Xerrors, fmt='none')
+    ax[pos].set_ylabel("Q2X of Imputation")
+    ax[pos].set_xlabel("Number of Components")
+    ax[pos].set_xticks([x for x in comps])
+    ax[pos].set_xticklabels([x for x in comps])
+    ax[pos].set_ylim(bottom=0.0, top=1.0)
 
 
-def q2xentry(ax, decomp):
-    # figure 3b in MSB
-    single_df = decomp.entryQ2X
+def q2xentry(ax, pos, decomp, methodname = "CP"):
+    entry_df = pd.DataFrame(decomp.entryQ2X).T
+    entrypca_df = pd.DataFrame(decomp.entryQ2XPCA).T
     comps = decomp.rrs
-    single_df = single_df.groupby(['Components']).agg(['mean', 'sem'])
 
-    CMTFR2X = single_df['CMTF']['mean']
-    CMTFErr = single_df['CMTF']['sem']
-    PCAR2X = single_df['PCA']['mean']
-    PCAErr = single_df['PCA']['sem']
-    ax.plot(comps - 0.1, CMTFR2X, ".", label="CMTF")
-    ax.plot(comps + 0.1, PCAR2X, ".", label="PCA")
-    ax.errorbar(comps - 0.1, CMTFR2X, yerr=CMTFErr, fmt='none', ecolor='b')
-    ax.errorbar(comps + 0.1, PCAR2X, yerr=PCAErr, fmt='none', ecolor='darkorange')
-    ax.set_ylabel("Q2X of Imputation")
-    ax.set_xlabel("Number of Components")
-    ax.set_xticks([x for x in comps])
-    ax.set_xticklabels([x for x in comps])
-    ax.set_ylim(0, 1)
-    ax.legend(loc=4)
+    entry_df.index = comps
+    entry_df['mean'] = entry_df.mean(axis=1)
+    entry_df['sem'] = entry_df.sem(axis=1)
+    entrypca_df.index = comps
+    entrypca_df['mean'] = entrypca_df.mean(axis=1)
+    entrypca_df['sem'] = entrypca_df.sem(axis=1)
 
-    pass
+    TR2X = entry_df['mean']
+    TErr = entry_df['sem']
+    PCAR2X = entrypca_df['mean']
+    PCAErr = entrypca_df['sem']
+    ax[pos].plot(comps - 0.1, TR2X, ".", label=methodname)
+    ax[pos].plot(comps + 0.1, PCAR2X, ".", label="PCA")
+    ax[pos].errorbar(comps - 0.1, TR2X, yerr=TErr, fmt='none', ecolor='b')
+    ax[pos].errorbar(comps + 0.1, PCAR2X, yerr=PCAErr, fmt='none', ecolor='darkorange')
+    ax[pos].set_ylabel("Q2X of Imputation")
+    ax[pos].set_xlabel("Number of Components")
+    ax[pos].set_xticks([x for x in comps])
+    ax[pos].set_xticklabels([x for x in comps])
+    ax[pos].set_ylim(0, 1)
+    ax[pos].legend(loc=4)
 
 
 def plot_weights(decomp):
