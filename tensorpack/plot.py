9de50f508f62920e39cb03b3d1720e793631e4d4
jstickelmaier <jamie.stickelmaier@gmail.com>
2022-04-08 13:06:04 -0700

    generate weight plot

--- tensorpack/plot.py
+++ tensorpack/plot.py
@@ -6,6 +6,9 @@ import numpy as np
 import pandas as pd
 from matplotlib.ticker import ScalarFormatter
 from .decomposition import Decomposition
+from tensorpack import perform_CP
+import seaborn as sns
+from matplotlib import gridspec, pyplot as plt
 
 
 def tfacr2x(ax, decomp:Decomposition):
@@ -124,6 +127,42 @@ def q2xentry(ax, decomp, methodname = "CP"):
     ax.legend(loc=4)
 
 
-def plot_weights(ax, pos, decomp):
-    # figure 5 in MSB
-    pass
+def plot_weights(ax, tensor, axes):
+    """
+    Plots heatmaps for each input dimension by component.
+
+    Parameters
+    ----------
+    ax : axis object
+        Plot information for a subplot of figure f.
+    tensor : numpy ndarray 
+        Takes a tensor to perform CP on.
+    axes : tuple of 3 lists
+        Allows for naming graph axes. 
+    """
+    subjects, receptors, antigens = axes
+    tfac = perform_CP(tOrig=tensor)
+
+    components =  [str(ii + 1) for ii in range(tfac.rank)]
+
+    subs = pd.DataFrame(tfac.factors[0], columns=[f"Cmp. {i}" for i in np.arange(1, tfac.rank + 1)], index=subjects)
+    rec = pd.DataFrame(tfac.factors[1], columns=[f"Cmp. {i}" for i in np.arange(1, tfac.rank + 1)], index=receptors)
+    ant = pd.DataFrame(tfac.factors[2], columns=[f"Cmp. {i}" for i in np.arange(1, tfac.rank + 1)], index=antigens)
+    
+    gs = gridspec.GridSpec(1, 3, wspace=0.5)
+    ax1 = plt.subplot(gs[0])
+    ax2 = plt.subplot(gs[1])
+    ax3 = plt.subplot(gs[2])
+
+    sns.heatmap(subs, cmap="PiYG", center=0, xticklabels=components, cbar=True, vmin=-1.0, vmax=1.0, ax=ax1)
+    sns.heatmap(rec, cmap="PiYG", center=0, xticklabels=components, yticklabels=receptors, cbar=False, vmin=-1.0, vmax=1.0, ax=ax2)
+    sns.heatmap(ant, cmap="PiYG", center=0, xticklabels=components, yticklabels=antigens, cbar=False, vmin=-1.0, vmax=1.0, ax=ax3)
+
+    ax1.set_xlabel("Components")
+    ax1.set_title("Subjects")
+    ax2.set_xlabel("Components")
+    ax2.set_title("Receptors")
+    ax3.set_xlabel("Components")
+    ax3.set_title("Antigens")
+
+
