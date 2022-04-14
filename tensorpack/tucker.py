56237831179768315d644c2d25f373f30803ae5b
Farnaz Mohammadi <farnazmd95@gmail.com>
2022-04-13 18:00:58 -0700

    more efficient tucker

--- tensorpack/tucker.py
+++ tensorpack/tucker.py
@@ -5,29 +5,6 @@ import numpy as np
 import itertools as it
 import tensorly as tl
 
-def tucker_rank(least_comps: int, most_comps: int):
-    """ Create list of tuples to pass to tucker as the rank. Depending on the number of dimensions of the tensor.
-    :param least_comps: least number of total components = the number of dimensions of the tensor.
-    :param most_comps: most number of total components = highest_rank - 1.
-    :return ranks: list of lists of permutations of ranks for each sum of components.
-    Example:
-    least_comps = 3, most_comps = 5
-    -> return [ [(1, 1, 1)], 
-                [(1, 1, 2), (1, 2, 1), (2, 1, 1)] ]
-    """
-
-    ranks = []
-    for i in range(least_comps, most_comps):
-        # combinations
-        choices = [pair for pair in it.combinations_with_replacement(range(1, most_comps-1), least_comps) if sum(pair) == i]
-        permutation = []
-        # permutations of the choices, remove duplicates
-        for sets in choices:
-            permutation.append(list(set([pars for pars in it.permutations(sets)])))
-        ranks.append(list(it.chain(*permutation)))
-
-    return ranks
-
 def perform_tucker(tensor, num_comps: int):
     """ Performs Tucker decomposition.
 
@@ -48,33 +25,32 @@ def perform_tucker(tensor, num_comps: int):
         list of the corresponding ranks combinations for the minimum error.
     """
 
-    ranks = tucker_rank(tensor.ndim, num_comps+1)
-
     # if tensor is xarray...
     if type(tensor) is not np.ndarray:
         tensor = tensor.to_numpy()
 
-    min_err = []
-    min_err_rank = []
-    factors = []
 
-    for total_rank in ranks:
-        error = []
-        facs = []
+    # step 1 with 1 component along every dimension
+    start = [1] * tensor.ndim
+    factors = [tucker(tensor, rank=start, svd='randomized_svd')]
+    min_err = [(tl.norm(tl.tucker_to_tensor(factors[0]) - tensor) ** 2) / tl.norm(tensor) ** 2]
+    min_rank = [start]
+    ranks = min_rank * tensor.ndim
 
-        for eachCP_rank in total_rank:
-            facs.append(tucker(tensor, rank=eachCP_rank, svd='randomized_svd'))
+    for _ in range(num_comps+1):
+
+        fac = []
+        err = []
+        for indx, val in enumerate(ranks):
+            ranks[indx][indx] += 1
 
             # calculate error for this rank combination
-            error.append((tl.norm(tl.tucker_to_tensor(facs[-1]) - tensor) ** 2) / tl.norm(tensor) ** 2)
+            fac.append(tucker(tensor, rank=ranks[indx][indx], svd='randomized_svd'))
+            err.append((tl.norm(tl.tucker_to_tensor(fac[-1]) - tensor) ** 2) / tl.norm(tensor) ** 2)
 
-        # append the least error of total_rank
-        min_err.append(min(error))
+        # pick the lowest error and continue with that
+        min_err.append(min(err))
+        min_rank.append(ranks[err.index(min(err))])
+        factors.append(fac[err.index(min(err))])
 
-        # append the rank combination for all dimensions for the min error
-        min_err_rank.append(total_rank[error.index(min(error))])
-
-        # append the tucker decomposition object for the minimum error for a specific combination.
-        factors.append(facs[error.index(min(error))])
-
-    return factors, min_err, min_err_rank
+    return factors, min_err, min_rank
