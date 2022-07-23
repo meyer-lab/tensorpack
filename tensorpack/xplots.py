6622dabfe07b63ff37a95d0643bfc85d89c5a2e8
Cyrillus <cyztan@gmail.com>
2022-07-22 19:09:23 -0700

    Add reorder option to component plot

--- tensorpack/xplots.py
+++ tensorpack/xplots.py
@@ -4,6 +4,7 @@ import numpy as np
 from .cmtf import perform_CP
 from matplotlib import gridspec, pyplot as plt
 import seaborn as sns
+import scipy.cluster.hierarchy as sch
 from .decomposition import Decomposition
 
 def xplot_reduction(data:xr.DataArray):
@@ -11,7 +12,7 @@ def xplot_reduction(data:xr.DataArray):
     pass
 
 
-def xplot_components(data:xr.DataArray, rank: int):
+def xplot_components(data:xr.DataArray, rank: int, reorder=[]):
     """ Plot the heatmaps of each components from an xarray-formatted data. """
     cp = perform_CP(data.to_numpy(), rank)
     ddims = len(data.coords)
@@ -19,6 +20,15 @@ def xplot_components(data:xr.DataArray, rank: int):
 
     factors = [pd.DataFrame(cp[1][rr], columns=[f"Cmp. {i}" for i in np.arange(1, rank + 1)],
                             index=data.coords[axes_names[rr]]) for rr in range(ddims)]
+
+    for r_ax in reorder:
+        if isinstance(r_ax, int):
+            assert r_ax < ddims
+            factors[r_ax] = reorder_table(factors[r_ax])
+        elif isinstance(r_ax, str):
+            assert r_ax in axes_names
+            rr = axes_names.index(r_ax)
+            factors[rr] = reorder_table(factors[rr])
 
     f = plt.figure(figsize=(5*ddims, 6))
     gs = gridspec.GridSpec(1, ddims, wspace=0.5)
@@ -31,3 +41,18 @@ def xplot_components(data:xr.DataArray, rank: int):
         axes[rr].set_xlabel("Components")
         axes[rr].set_title(axes_names[rr])
     return f
+
+
+def reorder_table(df):
+    """
+    Reorder a table's rows using hierarchical clustering.
+    Parameters:
+        df (pandas.DataFrame): data to be clustered; rows are treated as samples
+            to be clustered
+    Returns:
+        df (pandas.DataFrame): data with rows reordered via heirarchical
+            clustering
+    """
+    y = sch.linkage(df.to_numpy(), method='centroid')
+    index = sch.dendrogram(y, orientation='right', no_plot=True)['leaves']
+    return df.iloc[index, :]
