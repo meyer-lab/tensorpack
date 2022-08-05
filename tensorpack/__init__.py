e90a0ce78b43af7f1b61f3a8398981305ba1f0d1
Cyrillus <cyztan@gmail.com>
2022-08-05 14:42:17 -0700

    Version number change

--- tensorpack/__init__.py
+++ tensorpack/__init__.py
@@ -1,6 +1,7 @@
-__version__ = '0.0.1'
+__version__ = '0.1.0'
 
 from .cmtf import *
 from .decomposition import Decomposition
 from .tucker import tucker_decomp
 from .xplots import xplot_components
+from .coupled import CoupledTensor
