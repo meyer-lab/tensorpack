e5cce372e6e018cea76384f7a9f0f323faedf8f2
Cyrillus <cyztan@gmail.com>
2022-09-21 16:37:43 -0700

    Increment version number; remove unneeded method comparison files

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ except ImportError:
     from distutils.core import setup, find_packages
 
 setup(name='tensorpack',
-      version='0.1.0',
+      version='0.1.1',
       description='A collection of tensor methods from the Meyer lab.',
       url='https://github.com/meyer-lab/tensorpack',
       license='MIT',
