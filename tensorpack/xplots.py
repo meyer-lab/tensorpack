430ea5bcf328c1eb147d91bcd44428c7f8d9e309
ctan <cyztan@gmail.com>
2023-06-02 19:43:30 -0700

    Add a simple standard R2X plot for xplot

--- tensorpack/xplots.py
+++ tensorpack/xplots.py
@@ -1,15 +1,37 @@
 import pandas as pd
 import xarray as xr
 import numpy as np
-from .cmtf import perform_CP
+from .cmtf import perform_CP, calcR2X
+
 from matplotlib import gridspec, pyplot as plt
+from matplotlib.ticker import MaxNLocator
 import seaborn as sns
 import scipy.cluster.hierarchy as sch
-from .decomposition import Decomposition
 
-def xplot_reduction(data:xr.DataArray):
-    """ Plot increasing rank R2X and data reduction of PCA/CP/Tucker """
-    pass
+def xplot_R2X(data:xr.DataArray, top_rank=12, ax=None, method=perform_CP):
+    """ Plot increasing rank R2X for CP """
+    assert isinstance(data, xr.DataArray) or isinstance(data, np.ndarray)
+    ranks = np.arange(1, min(np.min(data.shape), top_rank)+1)
+    R2Xs = []
+
+    for r in ranks:
+        cp = method(data.to_numpy(), r)
+        R2Xs.append(calcR2X(cp, tIn=data.to_numpy()))
+
+    plt_indep = ax is None
+    if plt_indep:
+        f = plt.figure(figsize=(5, 4))
+        gs = gridspec.GridSpec(1, 1, wspace=0.5)
+        ax = plt.subplot(gs[0])
+
+    ax = sns.lineplot(x=ranks, y=R2Xs, ax=ax)
+    ax.set_ylim(0, 1)
+    ax.set_xlabel("Number of components")
+    ax.xaxis.set_major_locator(MaxNLocator(integer=True))
+    ax.set_ylabel("R2X")
+    ax.set_title("Variance Explained by CP")
+
+    return (f, ax) if plt_indep else ax
 
 
 def xplot_components(data:xr.DataArray, rank: int, reorder=[]):
